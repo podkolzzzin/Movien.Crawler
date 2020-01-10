92e8a21458ac699ff014ce09cdfb23d6738a88a7
Кипятков Вадим <dimaskripka1992@gmail.com>
2020-01-11 01:28:56 +0200

    неанеа

--- Movien.Crawler.LostFilm/Services/LoadFilmParserService.cs
+++ Movien.Crawler.LostFilm/Services/LoadFilmParserService.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Movien.Crawler.LostFilm.Services {
 
@@ -30,15 +31,28 @@ namespace Movien.Crawler.LostFilm.Services {
         ParseHTML(obj);
     }
 
-    private void ParseHTML(ContentLoadedEvent obj) {
+        private void ParseHTML(ContentLoadedEvent obj)
+        {
 
-      HtmlDocument doc = new HtmlDocument();
-      doc.LoadHtml(obj.Content);
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(obj.Content);
 
-      // TODO Extract all links that
-      // looks like: http://www.lostfilm.tv/series/<SERIES_NAME>/season_<SESAON_NUMBER>/episode_<EPISODE_NUMBER>/
-      // Publish them as LoadPageEvents
-    }
+            // TODO Extract all links that
+            // looks like: http://www.lostfilm.tv/series/<SERIES_NAME>/season_<SESAON_NUMBER>/episode_<EPISODE_NUMBER>/
+            // Publish them as LoadPageEvents
+
+            Regex regex = new Regex(@"((?:https|http):\/\/)\w{0,3}.lostfilm.\w+\/\w+\S+.\/season_\w{0,3}.\/episode_\w{0,3}.\/");
+
+            List<LoadPageEvent> links = new List<LoadPageEvent>();
+            
+            MatchCollection matches = regex.Matches(obj.Content);
+
+            foreach (var match in matches)
+            {
+                queue.Publish(new LoadPageEvent(new Uri(match.ToString()), HttpMethod.Get, null));
+            }
+
+        }
 
     private void ParseJSON(ContentLoadedEvent obj) {
 
